b83016281da5ff5562cecadcf5eeb653db1ea228
medetku <m.kuzhebayev@gmail.com>
2021-09-16 14:11:03 +0000

    change Pinger

--- ping.py
+++ ping.py
@@ -1,6 +1,11 @@
 import subprocess
+import sys
 
-ip_file_name = 'iplist.txt'
+if len(sys.argv) == 1:
+    print('Вы не указали имя файла с ip адресами. Выхожу ...')
+    quit()
+
+ip_file_name = sys.argv[1]
 pingable_file_name = 'pingable.txt'
 unpingable_file_name = 'unpingable.txt'
 
@@ -11,10 +16,16 @@ try:
         for ip in fip:
             ip = ip.strip()
             command = ['ping', '-c', '1', ip]
-            exit_code = subprocess.call(command)
+            exit_code = subprocess.call(
+                    command,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                    )
             if exit_code == 0:
                 fable.write(ip + '\n')
+                print(ip, '- OK')
             else:
                 funable.write(ip + '\n')
+                print(ip, '- FAIL')
 except FileNotFoundError as e:
     print('warning: can\'t find file {}'.format(e.filename))
